091d6384ef7418a8ccdc8ac1b3d2d6a2348e5436
Seonwoong Yoon <remy2019@gmx.us>
2023-10-14 14:56:49 +0900

    add concurrent mechanism for price_gen_daily.sh

--- price/price_gen_daily.sh
+++ price/price_gen_daily.sh
@@ -1,7 +1,7 @@
 #! /bin/bash
 
-# summary: generate raw stock prices A, B, C with different characteristics
-#        : then, rearrange the csv files for visualization
+# summary: generate raw stock prices A, B, C with different characteristics concurrently
+#        : wait. then, rearrange the csv files for visualization
 #
 # this script will run in the following cases:
 #   1. when Docker initiates (for initialization)
@@ -14,19 +14,31 @@
 start=$(date +'%Y-%m-%d')
 
 # generate raw stockA price
-pricegenerator --timeframe 1 --horizon 1440 --start "$start 00:00" --max-close 100 --min-close 50 --trend-deviation 0.03 --outliers-prob 0.05 --generate --save-to csv/rawA.csv
+pricegenerator --timeframe 1 --horizon 1440 --start "$start 00:00" --max-close 100 --min-close 50 --trend-deviation 0.03 --outliers-prob 0.05 --generate --save-to csv/rawA.csv &
+P1=$!
 
-# generate raw stockB price
-pricegenerator --timeframe 1 --horizon 1440 --start "$start 00:00" --max-close 100 --min-close 50 --trend-deviation 0.03 --outliers-prob 0.05 --generate --save-to csv/rawB.csv
+# generate raw stockB price concurrently
+pricegenerator --timeframe 1 --horizon 1440 --start "$start 00:00" --max-close 100 --min-close 50 --trend-deviation 0.03 --outliers-prob 0.05 --generate --save-to csv/rawB.csv &
+P2=$!
 
-# generate raw stockC price
-pricegenerator --timeframe 1 --horizon 1440 --start "$start 00:00" --max-close 100 --min-close 50 --trend-deviation 0.03 --outliers-prob 0.05 --generate --save-to csv/rawC.csv
+# generate raw stockC price concurrently
+pricegenerator --timeframe 1 --horizon 1440 --start "$start 00:00" --max-close 100 --min-close 50 --trend-deviation 0.03 --outliers-prob 0.05 --generate --save-to csv/rawC.csv &
+P3=$!
+
+# wait for concurrent jobs to finish
+wait $P1 $P2 $P3
 
 # rearrange rawA.csv to cookedA.csv
-./price_history_all.out csv/rawA.csv csv/cookedA.csv
+./price_history_all.out csv/rawA.csv csv/cookedA.csv &
+P4=$!
 
 # rearrange rawB.csv to cookedB.csv
-./price_history_all.out csv/rawB.csv csv/cookedB.csv
+./price_history_all.out csv/rawB.csv csv/cookedB.csv &
+P5=$!
 
 # rearrange rawC.csv to cookedC.csv
-./price_history_all.out csv/rawC.csv csv/cookedC.csv
+./price_history_all.out csv/rawC.csv csv/cookedC.csv &
+P6=$!
+
+# wait for concurrent jobs to finish
+wait $P4 $P5 $P6
