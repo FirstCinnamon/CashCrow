8a1e2468ad6053d9fa50932a97833b412a0dc3f2
bacon8282 <bacon8282@naver.com>
2023-10-27 23:01:28 +0900

    for select

--- db/main.cpp
+++ db/main.cpp
@@ -24,7 +24,17 @@ namespace db
             pqxx::params param(id);
             pqxx::result result = w->exec_prepared("select_from_owner", param);
             pqxx::row r = result.at(0);
-            int ret = r.at("account_balance").as<int>();
+            int ret = r[1].as<int>();
+            w->commit();
+            return ret;
+        }
+
+        std::tuple<int, std::string, int> selectFromBankAccount(int id) {
+            pqxx::params param(id);
+            pqxx::result result = w->exec_prepared("select_from_bank_account", param);
+            pqxx::row r = result.at(0);
+            std::tuple<int, std::string, int> ret;
+            ret = std::make_tuple(r[1].as<int>(), r[2].as<std::string>(), r[3].as<int>());
             w->commit();
             return ret;
         }
@@ -38,17 +48,6 @@ namespace db
         void insertBankAccount(int owner_id, const std::string& bank_name, int balance)
         {
             pqxx::params param(owner_id, bank_name, balance);
-            pqxx::result result = w->exec_prepared("insert_bank_account", param);
-            w->commit();
-        }
-
-        void getBankAccount(int owner_id, const std::string& bank_name, int balance)
-        {
-            pqxx::params param;
-            param.append(bank_name);
-            std::string sql = std::format("INSERT INTO bank_account (id, owner_id, bank_name, balance) SELECT COALESCE(MAX(id), 0) + 1, {}, '{}', {} FROM bank_account;",
-                owner_id, bank_name, balance);
-            w->exec0(sql);
             pqxx::result result = w->exec_prepared("insert_bank_account", param);
             w->commit();
         }
@@ -72,7 +71,11 @@ namespace db
         void prepare() {
             w->exec("PREPARE insert_bank_account AS INSERT INTO bank_account(id, owner_id, bank_name, balance) SELECT COALESCE(MAX(id), 0) + 1, $1, $2, $3 FROM bank_account;");
             w->exec("PREPARE insert_owner AS INSERT INTO owner (id, account_balance) SELECT COALESCE(MAX(id), 0) + 1, $1 FROM owner;");
-            w->exec("PREPARE select_from_owner AS SELECT * FROM owner WHERE id = 3;");
+            //w->exec("PREPARE insert_trade_history AS INSERT INTO trade_history (id, product, time_traded, price, buyer_id) SELECT COALESCE(MAX(id), 0) + 1, $1, $2, $3, $4 FROM owner;");
+
+
+            w->exec("PREPARE select_from_owner AS SELECT * FROM owner WHERE id = $1;");
+            w->exec("PREPARE select_from_bank_account AS SELECT * FROM bank_account WHERE id = $1;");
         }
     };
 
@@ -81,12 +84,15 @@ namespace db
 int main() {
     db::DBConnection con("host=localhost user=postgres dbname=postgres password=PASSWORD port=5432 connect_timeout=10");
 
-    con.insertOwner(20000);
+    auto ret = con.selectFromBankAccount(1);
+    //con.insertOwner(20000);
     //pqxx::row r = con.w->exec1("SELECT name from game limit 1");
 
     //con.w->commit();
 
-    //std::cout << r[0].as<std::string>() << std::endl;
+    std::cout << std::get<0>(ret) << std::endl;
+    std::cout << std::get<1>(ret) << std::endl;
+    std::cout << std::get<2>(ret) << std::endl;
     return 0;
 }
 
